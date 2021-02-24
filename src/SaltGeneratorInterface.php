b1ca17991674816560cfe65c79d23f0d1b6a33af
Ambroise Maupate <ambroise@rezo-zero.com>
2021-02-24 11:56:20 +0100

    First commit

---
+++ src/SaltGeneratorInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace RZ\Roadiz\Random;
+
+interface SaltGeneratorInterface
+{
+    /**
+     * @return string
+     */
+    public function generateSalt();
+}
---
+++ src/TokenGeneratorInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace RZ\Roadiz\Random;
+
+interface TokenGeneratorInterface
+{
+    /**
+     * @return string
+     */
+    public function generateToken();
+}
