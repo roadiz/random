5437da4f0b961b32f2c18dcc5a5e560c46f4ae8b
roadiz-ci <ambroise+ci@roadiz.io>
2025-09-10 10:20:07 +0000

    chore: Bumped

--- src/SaltGenerator.php
+++ src/SaltGenerator.php
@@ -6,8 +6,10 @@ namespace RZ\Roadiz\Random;
 
 class SaltGenerator extends RandomGenerator implements SaltGeneratorInterface
 {
-    #[\Override]
-    public function generateSalt(): string
+    /**
+     * @return string
+     */
+    public function generateSalt()
     {
         return strtr(base64_encode($this->getRandomNumber(24)), '{}', '-_');
     }
