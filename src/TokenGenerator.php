eac97739df8af8f18b0d1f1ed70a7a0bf36f7339
roadiz-ci <ambroise+ci@roadiz.io>
2026-07-03 13:10:48 +0000

    chore: bumped

--- src/TokenGenerator.php
+++ src/TokenGenerator.php
@@ -6,7 +6,10 @@ namespace RZ\Roadiz\Random;
 
 class TokenGenerator extends RandomGenerator implements TokenGeneratorInterface
 {
-    public function generateToken(): string
+    /**
+     * @return string
+     */
+    public function generateToken()
     {
         return rtrim(strtr(base64_encode($this->getRandomNumber()), '+/', '-_'), '=');
     }
